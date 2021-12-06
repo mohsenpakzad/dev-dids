3942347ef5f57791db32dccaefa2128295f32b0c
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-06 19:22:24 +0330

    Create base structure of project

---
+++ contracts/DevDIDs.sol
@@ -0,0 +1,14 @@
+// Be name khoda
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "./interfaces/IHolder.sol";
+import "./interfaces/IIssuer.sol";
+import "./interfaces/IVerifier.sol";
+
+contract DevDIDs is ERC721, IHolder, IIssuer, IVerifier {
+
+}
