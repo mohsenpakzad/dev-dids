059aff21320ae359d2b99d8b6d82b46fb3672984
Mohammad Amin Baghbanbashi <44613011+OoAminoO@users.noreply.github.com>
2021-12-07 12:24:24 -0800

    Balance and VC test

--- test/index.ts
+++ test/index.ts
@@ -12,6 +12,30 @@ describe("DevDIDs", function () {
   });
 
   describe("Issuer", async () => {
+    it("The Balance should be equal to three", async function(){
+      let owner, addr1,addr2,addrs;
+      [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+      devDIDs.issue(addr1.address,"ZahraMohammadPour","Has compeleted first sprint",5,20);
+      devDIDs.issue(addr1.address,"ZahraMohammadPour","Has got driving license",5,20);
+      devDIDs.issue(addr1.address,"ZahraMohammadPour","Has compeleted her course",5,20);
+
+      expect(await devDIDs.balanceOf(addr1.address)).to.equal(3);
+
+
+    });
+    it("The number of holder vcs should be equal to three", async function(){
+    let owner, addr1,addr2,addrs;
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    devDIDs.issue(addr1.address,"ZahraMohammadPour","Has compeleted first sprint",5,20);
+    devDIDs.issue(addr1.address,"ZahraMohammadPour","Has got driving license",5,20);
+    devDIDs.issue(addr1.address,"ZahraMohammadPour","Has compeleted her course",5,20);
+
+    expect((await devDIDs.vcsOfHolder(addr1.address)).length).to.equal(3);
+
+    });
+    
     it("should return address of owner of vc1 addr1", async function () {
       const [, addr1] = await ethers.getSigners();
 
