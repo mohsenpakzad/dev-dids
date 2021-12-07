a67621d0f2a5a82bd31d87414d44277193cca351
shima78 <shi1999mamp@gmail.com>
2021-12-07 17:59:53 +0330

    Issuance test

--- test/index.ts
+++ test/index.ts
@@ -2,18 +2,18 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("DevDIDs", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Should issue a Vc for address", async function () {
     const DevDIDs = await ethers.getContractFactory("DevDIDs");
     const devDIDs = await DevDIDs.deploy();
     await devDIDs.deployed();
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    // expect(await greeter.greet()).to.equal("Hello, world!");
-    //
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-    //
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-    //
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+
+    await devDIDs.issue(addr1.address,"ZahraMohammadPour","Has compeleted first sprint",5,20);
+    expect(await devDIDs.ownerOf(1)).to.equal(addr1.address);
   });
 });
