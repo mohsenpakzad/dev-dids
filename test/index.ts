7d48525a6d8d0fd764d17b0ed59b1c3b9d47c270
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-07 17:02:08 +0330

    Change testing contract from Greeter to DevDIDs

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,19 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
+describe("DevDIDs", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const DevDIDs = await ethers.getContractFactory("DevDIDs");
+    const devDIDs = await DevDIDs.deploy();
+    await devDIDs.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // expect(await greeter.greet()).to.equal("Hello, world!");
+    //
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    //
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+    //
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
