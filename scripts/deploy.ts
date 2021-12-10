11819a34833ffefb9283c6454fca1b884d0e5613
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-10 05:03:39 +0330

    Implement saveDeployedData functionality to deploy script and add deployedData to gitignore

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,8 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { artifacts, ethers } from "hardhat";
+import { BaseContract } from "ethers";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -23,6 +24,31 @@ async function main() {
   await devDIDs.deployed();
 
   console.log("DevDIDs deployed to:", devDIDs.address);
+
+  saveDeployedData(devDIDs);
+}
+
+function saveDeployedData(contract: BaseContract) {
+  const fs = require("fs");
+  const path = require("path");
+
+  const contractsDir = path.join(__dirname, "..", "deployedData");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/contract-address.json",
+    JSON.stringify({ DevDIDs: contract.address }, undefined, 2)
+  );
+
+  const DevDIDsArtifact = artifacts.readArtifactSync("DevDIDs");
+
+  fs.writeFileSync(
+    contractsDir + "/DevDIDs.json",
+    JSON.stringify(DevDIDsArtifact, null, 2)
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
